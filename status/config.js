8aaee406fe4fbc879b289cbb7cc6ad14412da4ea
“ALBULAING” <LL2533075229@outlook.com>
2024-10-29 22:06:38 +0800

    Site updated: 2024-10-29 22:06:36

---
+++ status/config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '网站服务状态检测',
+
+  // UptimeRobot API Keys
+  // 支持Monitor-Specific API Keys和Read-Only API Key，推荐使用Read-Only API Key，可填写多个
+  ApiKeys: [
+    'ur2631068-c5d3500ed9a3711613eb8190',
+  ],
+
+  // 日志天数，建议90
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: false,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://home.cool.cc.ua/'
+    },
+    {
+      text: '个人博客',
+      url: 'https://blog.coolspace.cc/'
+    },
+  ],
+};
